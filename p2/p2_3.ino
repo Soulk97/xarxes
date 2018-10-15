a728504310044db5d7cff323eea924296a4d4cb8
Soulk97 <david.martin2897@gmail.com>
2018-10-15 18:07:51 +0200

    codis 3-4 p2

---
+++ p2/p2_3.ino
@@ -0,0 +1,33 @@
+void connectToWiFi(void){
+    if (WiFi.status() != Wl_CONNECTED){
+        WiFi.begin(c_ssid, c_pwd);
+        while (WiFi.status() != WL_CONNECTED){
+            delay(1000);
+            Serial.println("Connecting...");
+            contador++;
+            if(contador ==10){
+                break;
+            }
+        }
+
+        Serial.println(WiFi.localIP());
+    }
+
+}
+
+void wifiNetworkSelection(void){
+    if(WiFi.status() != WL_CONNECTED){
+        Serial.println("SSID? >");
+        while(!Serial.available());
+        ssid = Serial.readString();
+        Serial.print("SSID_selected > ");
+        Serial.println(ssid);
+        delay(1000);
+        Serial.print("Password? > ");
+        while(!Serial.available());
+        password = Serial.readString();
+        Serial.print("PWD > ");
+        Serial.println(password);
+        delay(1000);
+    }
+}
