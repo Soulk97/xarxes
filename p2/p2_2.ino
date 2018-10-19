3065f60a6514b784e2b188c2182b5a517b606f9c
Soulk97 <david.martin2897@gmail.com>
2018-10-19 11:43:05 +0200

    dsaajkfd

--- p2/p2_2.ino
+++ p2/p2_2.ino
@@ -6,8 +6,71 @@ void setup() {
   WiFi.disconnect();
   delay(2000);
   Serial.println("Setup done");
+
+  wifiNetworkSelection();
   
 }
+
+void connectToWiFi(String ssid, String pwd){
+    Serial.println("connectToWiFi");
+    
+    if (WiFi.status() != WL_CONNECTED){
+      int contador = 0;
+      char *c_ssid, *c_pwd;
+      strTochar(ssid, c_ssid);
+      strTochar(pwd, c_pwd);
+      Serial.println("begin");
+      WiFi.begin(c_ssid, c_pwd);
+
+      while (WiFi.status() != WL_CONNECTED){
+        delay(1000);
+        Serial.println("Connecting...");
+        contador++;
+        if(contador ==10){
+          break;
+        }
+      }
+      Serial.println("Local IP:");
+      Serial.println(WiFi.localIP());
+    }
+
+}
+
+void wifiNetworkSelection(void){
+    String ssid, password;
+    if(WiFi.status() != WL_CONNECTED){
+        Serial.println("SSID? >");
+        while(!Serial.available());
+        ssid = Serial.readString();
+        Serial.print("SSID_selected > ");
+        Serial.println(ssid);
+        delay(1000);
+        Serial.print("Password? > ");
+        while(!Serial.available());
+        password = Serial.readString();
+        Serial.print("PWD > ");
+        Serial.println(password);
+        delay(1000);
+        connectToWiFi(ssid, password);
+    }
+}
+
+
+void strTochar(String txt, char* c){
+  if(WiFi.status() != WL_CONNECTED){
+    int len = txt.length();
+    Serial.println(len);
+    Serial.println("*************");
+    c = (char*)malloc(len);
+    for(int i = 0; i<len-1; i++){
+      c[i] = txt[i];
+      Serial.print(c[i]);
+      }
+    }
+  }
+
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -23,8 +86,11 @@ void loop() {
       Serial.print(WiFi.RSSI(i));
       Serial.print(")");
       Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*");
-      delay(10);
+      
+      delay(30);
     }
+    //wifiNetworkSelection();
+    
   }
   Serial.println("*********************************");
   delay(5000);
